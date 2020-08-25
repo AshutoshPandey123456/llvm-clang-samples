5f93e7be52ef8725348837544ccf1feb22b85e96
Ashutosh Pandey <ashutosh-cs026@dsce.edu.in>
2020-08-25 22:10:36 +0530

    Updating to make code work with current libtooling
    I cloned your repo, and when I made these changes the code worked correctly. I'll work through the other examples if you're still up for maintaining the repo.
    
    Just another person that learnt a lot from your blogs. Thank you!

--- src_clang/tooling_sample.cpp
+++ src_clang/tooling_sample.cpp
@@ -22,6 +22,7 @@
 #include "clang/Tooling/Tooling.h"
 #include "llvm/Support/raw_ostream.h"
 
+using namespace std;
 using namespace clang;
 using namespace clang::driver;
 using namespace clang::tooling;
@@ -40,12 +41,12 @@ public:
       IfStmt *IfStatement = cast<IfStmt>(s);
       Stmt *Then = IfStatement->getThen();
 
-      TheRewriter.InsertText(Then->getLocStart(), "// the 'if' part\n", true,
+      TheRewriter.InsertText(Then->getBeginLoc(), "// the 'if' part\n", true,
                              true);
 
       Stmt *Else = IfStatement->getElse();
       if (Else)
-        TheRewriter.InsertText(Else->getLocStart(), "// the 'else' part\n",
+        TheRewriter.InsertText(Else->getBeginLoc(), "// the 'else' part\n",
                                true, true);
     }
 
@@ -75,7 +76,7 @@ public:
       // And after
       std::stringstream SSAfter;
       SSAfter << "\n// End function " << FuncName;
-      ST = FuncBody->getLocEnd().getLocWithOffset(1);
+      ST = FuncBody->getEndLoc().getLocWithOffset(1);
       TheRewriter.InsertText(ST, SSAfter.str(), true, true);
     }
 
@@ -124,7 +125,7 @@ public:
                                                  StringRef file) override {
     llvm::errs() << "** Creating AST consumer for: " << file << "\n";
     TheRewriter.setSourceMgr(CI.getSourceManager(), CI.getLangOpts());
-    return llvm::make_unique<MyASTConsumer>(TheRewriter);
+    return std::make_unique<MyASTConsumer>(TheRewriter);
   }
 
 private:
